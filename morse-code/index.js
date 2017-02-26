a356c05e96fe683f9293a05e9a000c8362308a23
Nicholas Smith <nicholasparkersmith@gmail.com>
2017-02-26 18:12:33 -0500

    Switch to Callback processing.
    Ultimately, Proxy's proved unreliable, especially when using other array methods upon the proxied array. Easier to rely on core methods like `shift` to manage the different state of the application. Only first test is passing.

--- morse-code/index.js
+++ morse-code/index.js
@@ -1,58 +1,43 @@
 const transmitter = (options, originalCB) => {
-	const handler = {
-		set(target, key, value) {
-			target[key] = value;
-			processMessage(value);
-		},
-	};
-
-	const otherHandler = {
-		set(target, key, value) {
-			if ( key === 'length' && !value ) {
-				target[key] = value;
-				updateChars();
-				return true;
-			}
-			target[key] = value;
-			processChar();
-			return true;
-		},
-	};
-
 	const {
 		codes,
 		message,
 		timeouter,
 		toggle,
 	} = options;
-	let chars = new Proxy(message.split(''), handler);
-	let codeLength = [];//= new Proxy([], otherHandler);
-	let curLoop = codeLength.length
+	let letters = message.split('');
+	let curCodes = [];
+	let curLoop = curCodes.length
 
-	const updateChars = () => chars.shift();
-	const updateCodeLength = () => codeLength.shift();
-
-	const sendMessage = (item, length = 1) => {
+	const sendMessage = (item, length = 1, callback) => {
 		const lengths = item === '.' ? 1 : item === '-' ? 3 : length;
 		timeouter(function() {
 			toggle();
-			updateCodeLength();
+			callback();
 		}, lengths);
 	}
 
-	const processChar = (option = codeLength[0]) => {
-		if (codeLength.length === curLoop) return;
-		curLoop = codeLength.length
-		sendMessage(option);
+	const processCurCode = () => {
+		if (!curCodes.length) return processMessage();
+		const item = curCodes.shift();
+		var callback = function() {
+			if (curCodes.length > 0) {
+				//if there's more to the code, add a dot after
+				sendMessage('', 1, () => processCurCode() );
+			} else {
+				processCurCode();
+			}
+		};
+
+		return sendMessage(item, 1, callback);
 	}
 
+	// Process a single letter
 	const processMessage = prev => {
-		if (codeLength.length !== curLoop) {
-			sendMessage('', 1);
-		}
-		if (!chars.length) originalCB();
-		codeLength = new Proxy(codes[chars[0]].split(''), otherHandler);
-		processChar();
+		if (!letters.length) return originalCB();
+		curCodes = codes[letters.shift()].split('');
+		// Will do more when ther are more characters to process
+		processCurCode(curCodes);
 	}
 
 	toggle();
